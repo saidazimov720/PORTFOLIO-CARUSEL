7a5fb0a398c800810eff93e0f70d86ef3196b522
saidazimov720 <saidazimovnurbek01@gmail.com>
2024-06-11 10:24:05 +0500

    msNumber

--- js/script.js
+++ js/script.js
@@ -1,4 +1,16 @@
 (function(){
     var slidersContainer = document.querySelector('.slider-container');
     
+    var msNumbers = new momentumSliderMin({
+        el: slidersContainer, 
+        cssClass: 'ms--numbers',
+        range: [1, 4],
+        rangeContent: function(i){
+            return '0' + i;
+        }, 
+        style: {
+            transform: [{scale: [0.4, 1]}],
+            opacity:[0, 1]
+        },
+    })
 })
